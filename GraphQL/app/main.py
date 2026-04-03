a6e64f16563efd03d2943afbc3d3211cf122c833
Benjamin-Harris1 <beha1503@gmail.com>
2026-04-03 23:46:28 +0200

    Refactor FastAPI setup to integrate GraphQL schema and router for library API

--- GraphQL/app/main.py
+++ GraphQL/app/main.py
@@ -1,6 +1,14 @@
+import strawberry
 from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+from app.schema.query import Query
+from app.schema.mutation import Mutation
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+graphql_app = GraphQLRouter(schema)
 
 app = FastAPI(title="GraphQL API", version="0.1.0")
+app.include_router(graphql_app, prefix="/graphql")
 
 
 @app.get("/")
