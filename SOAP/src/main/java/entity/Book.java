b5bd867252ed986e023ade6e1676fe07a8f67dc5
Benjamin-Harris1 <beha1503@gmail.com>
2026-04-08 21:58:08 +0200

    Add @XmlAccessorType annotation to Author, Book, and Publisher entities because PanacheEntity mismatch with lombok getter/setter caused a serialization error

--- SOAP/src/main/java/entity/Book.java
+++ SOAP/src/main/java/entity/Book.java
@@ -3,11 +3,14 @@ package entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import lombok.*;
 
 @Entity
 @Table(name = "book")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Book extends PanacheEntity {
     @Column(nullable = false, length = 255)
     private String title;
--- SOAP/src/main/java/entity/Publisher.java
+++ SOAP/src/main/java/entity/Publisher.java
@@ -2,11 +2,14 @@ package entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import lombok.*;
 
 @Entity
 @Table(name = "publisher")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Publisher extends PanacheEntity {
     @Column(nullable = false, length = 40)
     private String name;
